d07f1c39834d0849e99cf27d6fcdb46651765d88
Bohdan Inhliziian <bohdan.inhliziian@kiwi.com>
2020-03-06 18:21:48 +0100

    delete unused

--- tester-gui/firefox-tests/app/config/config.py
+++
@@ -1,8 +0,0 @@
-# Logs
-DRIVER_LOG_PATH = 'logs/geckodriver.log'
-
-# Start test page
-WEB_INTERFACE_HOST = 'http://localhost'
-WEB_INTERFACE_PORT = '998'
-
-ADDONS_ABSOLUTE_PATH = '/Users/bohdaninhliziian/no_sync/baka_proj/testing-framework/tester-gui/firefox-tests/addons/'
