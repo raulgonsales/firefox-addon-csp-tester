b7de24c4057159ebc5aced8aac7c11332c9a2029
Bohdan Inhliziian <bohdan.inhliziian@kiwi.com>
2020-04-12 17:17:08 +0200

    create addon_tests table

---
+++ tester-gui/database/migrations/2020_04_12_145714_create_addon_tests_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddonTestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addon_tests', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('addon_id')->unsigned();
+            $table->string('type_name');
+        });
+
+        Schema::table('addon_tests', function (Blueprint $table) {
+            $table->foreign('addon_id')
+                ->references('id')
+                ->on('addons');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('test_types');
+    }
+}
